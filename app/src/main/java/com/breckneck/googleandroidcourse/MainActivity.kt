c9f2fe04cc97734404d332a9bd08052d5680e82f
PavkaBrika <pavlikbrichkin@mail.ru>
2022-09-08 19:43:47 +0500

    created a contact card

--- app/src/main/java/com/breckneck/googleandroidcourse/MainActivity.kt
+++ app/src/main/java/com/breckneck/googleandroidcourse/MainActivity.kt
@@ -3,41 +3,47 @@ package com.breckneck.googleandroidcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.breckneck.googleandroidcourse.ui.theme.GoogleAndroidCourseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GoogleAndroidCourseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(msg = Message(first = "Android", second = "Jetpack Compose"))
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+data class Message(val first: String, val second: String)
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    GoogleAndroidCourseTheme {
-        Greeting("Android")
+fun MessageCard(msg: Message) {
+    Row(Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = "contact image",
+            modifier = Modifier
+                .size(75.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column() {
+            Text(text = msg.first)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = msg.second)
+        }
     }
+
 }
