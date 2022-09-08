dc71a5329bae2ed04e411267cbca57e0c561b8f7
PavkaBrika <pavlikbrichkin@mail.ru>
2022-09-08 21:48:46 +0500

    created a list of messages but problems with night theme

--- app/src/main/java/com/breckneck/googleandroidcourse/MainActivity.kt
+++ app/src/main/java/com/breckneck/googleandroidcourse/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.breckneck.googleandroidcourse
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -21,12 +25,118 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(msg = Message(first = "Android", second = "Jetpack Compose"))
+            GoogleAndroidCourseTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+//                    MessageCard(msg = Message(first = "Android", second = "Jetpack Compose"))
+                    Conversation(messages = SampleData.conversationSample)
+                }
+            }
         }
     }
 }
 
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Colleague",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Colleague",
+            "List of Android versions:\n" +
+                    "Android KitKat (API 19)\n" +
+                    "Android Lollipop (API 21)\n" +
+                    "Android Marshmallow (API 23)\n" +
+                    "Android Nougat (API 24)\n" +
+                    "Android Oreo (API 26)\n" +
+                    "Android Pie (API 28)\n" +
+                    "Android 10 (API 29)\n" +
+                    "Android 11 (API 30)\n" +
+                    "Android 12 (API 31)\n"
+        ),
+        Message(
+            "Colleague",
+            "I think Kotlin is my favorite programming language.\n" +
+                    "It's so much fun!"
+        ),
+        Message(
+            "Colleague",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Colleague",
+            "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            "Colleague",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Colleague",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Colleague",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Colleague",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Colleague",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Colleague",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Colleague",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Colleague",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
+
 data class Message(val first: String, val second: String)
+
+@Preview(name = "Light Mode", showBackground = true)
+@Preview(
+    name = "Dark Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(msg = Message("Android", "Jetpack Compose"))
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    GoogleAndroidCourseTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn() {
+        items(messages) { message ->  
+            MessageCard(msg = message)
+        }
+    }
+}
+
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -37,12 +147,23 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(75.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column() {
-            Text(text = msg.first)
+            Text(
+                text = msg.first,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.width(4.dp))
-            Text(text = msg.second)
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.second,
+                    style = MaterialTheme.typography.subtitle2,
+                    modifier = Modifier.padding(4.dp)
+                )
+            }
         }
     }
 
