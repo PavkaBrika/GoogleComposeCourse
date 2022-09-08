b32adaa176496aa45e62ac0affa16a3afb0ffad8
PavkaBrika <pavlikbrichkin@mail.ru>
2022-09-09 01:33:40 +0500

    added expandable column at on button click

--- app/src/main/java/com/breckneck/googleandroidcourse/SecondTask.kt
+++ app/src/main/java/com/breckneck/googleandroidcourse/SecondTask.kt
@@ -1,18 +1,19 @@
 package com.breckneck.googleandroidcourse
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun App() {
-    val list = listOf<String>("kotlin", "COmpose")
-    Column(modifier = Modifier.padding(4.dp)) {
+    val list = listOf<String>("Kotlin", "Compose")
+    Column(modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         for (item in list) {
             Tables(name = item)
         }
@@ -22,15 +23,26 @@ fun App() {
 
 @Composable
 fun Tables(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(4.dp)) {
-        Column(
-            modifier = Modifier
-                .padding(24.dp)
-                .fillMaxWidth()
-        ) {
-            Text(text = "HELLO", fontSize = 25.sp)
-            Text(text = name, fontSize = 25.sp)
+        Row(modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello,", fontSize = 25.sp)
+                Text(text = name, fontSize = 25.sp)
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value },
+            ) {
+                Text(text = if (expanded.value) "Show more" else "Show less")
+            }
         }
-        Spacer(modifier = Modifier.width(8.dp))
     }
 }
